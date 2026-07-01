1db1c4b6700f571d8ae87c5cfd47b974745d9636
DhruvNaina <dhruv4857.be21@chitkara.edu.in>
2026-07-01 12:06:08 +0530

    latest

--- frontend/brainly_frontend/my-app/src/App.tsx
+++ frontend/brainly_frontend/my-app/src/App.tsx
@@ -1,34 +1,50 @@
-import React from "react";
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import SignupPage from "./pages/Signup";
 import Login from "./pages/Login";
 import Real from "./pages/Real";
-import { useAuthStore } from "./store/auth.store";
 import Chat from "./pages/Chat";
+import { useAuthStore } from "./store/auth.store";
 
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const user = useAuthStore((store) => store.user);
 
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  return children;
+  return <>{children}</>;
 };
 
-const App: React.FC = () => {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<SignupPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/real" element={<Real />} />
-         <Route path="/chat" element={<Chat />} />
+
+        <Route
+          path="/real"
+          element={
+            <ProtectedRoute>
+              <Real />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
