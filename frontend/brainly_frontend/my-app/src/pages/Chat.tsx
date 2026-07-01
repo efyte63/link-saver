334889a8e01e5d223082065bb7f5f478e41fa60d
DhruvNaina <dhruv4857.be21@chitkara.edu.in>
2026-07-01 16:06:34 +0530

    latest

--- frontend/brainly_frontend/my-app/src/pages/Chat.tsx
+++ frontend/brainly_frontend/my-app/src/pages/Chat.tsx
@@ -1,18 +1,22 @@
-
-
 import Chatsidenavigation from '../component/Chatsidenavigation'
 import Chatbox from '../component/Chatbox'
+import { messageStore } from '../store/message'
 
 const Chat = () => {
+  const { selecteduser } = messageStore();
+
   return (
-    <div className='flex'>
+    <div className='flex h-screen w-full overflow-hidden'>
 
-        
+      <div className={`${selecteduser ? "hidden" : "flex"} md:flex w-full md:w-auto shrink-0 h-full`}>
         <Chatsidenavigation/>
-        <Chatbox/>
+      </div>
 
-        
-        </div>
+      <div className={`${selecteduser ? "flex" : "hidden"} md:flex flex-1 min-w-0 h-full`}>
+        <Chatbox/>
+      </div>
+
+    </div>
   )
 }
 
