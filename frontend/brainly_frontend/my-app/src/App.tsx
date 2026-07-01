c53a8e80ebfe406ab3adf18e35af5fa77e07ba19
DhruvNaina <dhruv4857.be21@chitkara.edu.in>
2026-07-01 12:06:27 +0530

    latest

--- frontend/brainly_frontend/my-app/src/App.tsx
+++ frontend/brainly_frontend/my-app/src/App.tsx
@@ -1,4 +1,4 @@
-
+import { ReactNode } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import SignupPage from "./pages/Signup";
